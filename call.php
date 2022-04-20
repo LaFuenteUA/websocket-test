361e4fb2839b441a8e7394283041600890eb33f1
LaFuenteUA <dobovo@kharkovhost.com>
2022-04-20 16:48:04 +0300

    Unused deleted

--- call.php
+++ call.php
@@ -4,7 +4,6 @@ if($data = $_REQUEST['data']) {
 	if(json_last_error() == JSON_ERROR_NONE) {
 		$redis = new Redis();
 		$redis->connect('127.0.0.1', 6379);
-		$redis->rawCommand('SET', 'msg_'.$id, $msg, 'EX', 20);
 		$redis->publish('png-news', json_encode($msg));
 	}
 }
